c0306ccce0ae55a4812b7c751827021c4ba6e4b8
Atom Yah <atom@yah>
2017-12-05 17:07:39 +0900

    p117

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 require_once __DIR__ .'/vendor/autoload.php';
 require __DIR__ . '/functions.php';
 
+define('TABLE_NAME_STONES', 'stones');
+
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
@@ -31,8 +34,10 @@ foreach ($events as $event) {
     continue;
   }
 
- // 石の配置
-  $stones =
+  
+  if(getStoneByUserID($event->getUserId()) === PDO::PARAM_NULL) { // もし初めてのユーザならば
+  // ゲーム開始時の石の配置
+    $stones =
           [
               [0,0,0,0,0,0,0,0,],
               [0,0,0,0,0,0,0,0,],
@@ -44,11 +49,48 @@ foreach ($events as $event) {
               [0,0,0,0,0,0,0,0,]             
           ];
   
+    registerUser($event->getUserId(), json_encode($stones));
+  
+    replyImagemap($bot, $event->getReplyToken(), '盤面', $stones);
+  
+    continue;
+  
+  } else {
+    $stones = getStonesByUserID($event->getUserId());
+  }
+  
   replyImagemap($bot, $event->getReplyToken(), '盤面', $stones);
+
+}  
+
+
+//ユーザをDBにインサートするファンクション
+function registerUser($userId, $stones) {
+  $dbh = dbConnection::getConnection();
+  $sql = 'INSERT INTO ' . TABLE_NAME_STONES .' (userid, stone) values (pgp_sym_encrypt(?, \'' . getenv('DB_ENCRYPT_PASS') . '\'), ?) ';
+  $sth = $dbh->prepare($sql);
+  $sth->execute(array($userId, $stones));
 }
 
 
+
+//ユーザーIDから、DBよりデータフェッチ
+function getStonesByUserId($userId) {
+  $dbh = dbConnection::getConnection();
+  $sql = 'SELECT stone from ' . TABLE_NAME_STONES . ' where ? = pgp_sys_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') .'\')';
+  $sth = $dbh->prepare($sql);
+  $sth->execute(array($userId));
   
+  if (!($row = $sth->fetch())) {
+    return PDO::PARAM_NULL;
+  } else {
+    return json_decode($row['stone']);
+  }
+}
+
+
+
+//イメージマップ作成ファンクション
   function replyImagemap($bot, $replyToken, $alternativeText, $stones) {
     $actionArray = array();
     
@@ -73,4 +115,26 @@ foreach ($events as $event) {
   }
 
 
+  //DB接続用クラス
+  class dbConnection {
+    protected static $db;
+    
+    private function __construct() {
+      try {
+          $url = parse_url(getenv('DATABASE_URL'));
+          $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+          $self::$db = new PDO($dsn, $url['user'], $url['pass']);
+          $self::$db->setAttribute(PDO::ATTR_ERRORMODE, PDO::ERRMODE_EXCEPTION);
+      } catch (PDOException $e) {
+          echo 'Connection Error: ' . $e->getMessage();
+      }
+    }
+    
+    public static function getConnection() {
+      if (!self::$db) {
+        new dbConnection();
+      }
+      return self::$db;
+    }
+  }
 ?>
