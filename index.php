0204dce1a553edc86f99be0fd9b44fd2e7a2a1dd
Atom Yah <atom@yah>
2017-12-10 16:47:49 +0900

    P130 placeAIStone()に引数をいれた

--- index.php
+++ index.php
@@ -83,7 +83,7 @@ foreach ($events as $event) {
   } else if(!getCanPlaceByColor($stones, true) && getCanPlaceByColor($stones, false)) {
     // ユーザーが置けるようになるまで相手が石を置く
     while(!getCanPlaceByColor($stones, true)) {
-      placeAIStone();
+      placeAIStone($stones);
       updateUser($bot, json_encode($stones));
       // どちらの石も置けなくなったらゲームオーバー
       if(!getCanPlaceByColor($stones, true) && !getCanPlaceByColor($stones, false)) {
